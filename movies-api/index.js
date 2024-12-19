7d45ba1e4360feed8a80ed5690aaf3c9f14ce70c
SineadCleary <20102299@mail.wit.ie>
2024-12-19 11:38:07 +0000

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler';
 import moviesRouter from './api/movies';   //import movies router
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
-app.use('/api/movies', moviesRouter); 
+app.use('/api/movies', authenticate, moviesRouter);
 
 app.use(defaultErrHandler);
 
